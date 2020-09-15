486ee160f23d14975493aada5eef444f7c649d07
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-15 17:54:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,9 +2,7 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-      binding.pry
-        puts "hello"
+    10.times do "s" + string
   else
     string
   end
