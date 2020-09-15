c00d9d4f6c4e49bc78d563dfa87b8d22122e841b
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-15 17:43:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,6 +1,6 @@
 # don't forget to add: require 'pry'
 def get_user_input
-  num = gets.chomp
+  gets.chomp
 end
 
 def prompt_user
@@ -11,9 +11,9 @@ end
 
 def selection(num)
   if num = 1
-     p "YUM YUM MUNCH MUNCH MUNCH"
+     "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     p "HAM HAM HAM IN MY TUMMY"
+     "HAM HAM HAM IN MY TUMMY"
   end
 end
 
