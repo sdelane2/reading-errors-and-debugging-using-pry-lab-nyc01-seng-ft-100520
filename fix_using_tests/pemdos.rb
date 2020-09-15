56235c9f306c082daf43db132bb8a97c8bc2a57f
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-15 17:59:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
-def snake_it_up(String)
-  if String[0] == "s"
-    10 * "s" + String
+def snake_it_up(string)
+  if string[0] == "s"
+    10 * "s" + string
   else
-    String
+    string
   end
 end
